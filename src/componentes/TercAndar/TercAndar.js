c8d1f8ebda63169063b020e31e4160c6789e34a0
Bruno Motta <100230939+Mottab766@users.noreply.github.com>
2024-11-06 16:01:33 -0300

    Add files via upload

--- src/componentes/TercAndar/TercAndar.js
+++ src/componentes/TercAndar/TercAndar.js
@@ -9,25 +9,50 @@ const Banner = styled.div`
 margin-top: 1rem;
 justify-content: center;
 background-color:#480078; 
-height: 6rem;`
+height: 6rem;
+
+@media (max-width: 768px) {
+  display: flex;
+ align-items: center;
+  height: 5rem; /* Ajusta a altura do banner em telas pequenas */
+}`
 
 const Titulo = styled.h2`
 color: #fff;
-font-size: 3rem`
+font-size: 3rem
+
+@media (max-width: 768px) {
+  font-size: 2rem; /* Reduz o tamanho do título em telas pequenas */
+}`
 
 const Personagens = styled.div`
 display:flex;
 justify-content:space-around;
 flex-direction:row;
-flex-wrap:wrap;`
+flex-wrap:wrap;
+
+@media (max-width: 768px) {
+  justify-content: center; /* Centraliza os personagens em telas pequenas */
+  flex-flow:wrap;
+}`;
 
 const Personagem = styled.div`
 display:flex;
 flex-direction:column;
-margin-left:10rem;
-margin-right:10rem;
+margin-left:6rem;
+margin-right:6rem;
+align-items: center;
 position: relative; /* Adicionar para controlar a posição do resumo */
-`
+
+@media (max-width: 768px) {
+  display: flex;
+  flex-flow:wrap;
+  flex-direction: column;
+  width:  70%; /* Ocupa toda a largura da tela em dispositivos pequenos */
+  height:  70%; /* Ocupa toda a largura da tela em dispositivos pequenos */
+  margin-left: 0; /* Reduz o espaço entre os personagens */
+  margin-right: 0; /* Reduz o espaço entre os personagens */
+}`
 
 
 const Imagem = styled.img`
@@ -36,7 +61,12 @@ margin-top: 2rem;
   height: 20rem;
   border-radius: 30%;
   transition: transform 0.1s ease-in-out; /* Transição para o efeito de hover */
-`;
+
+  @media (max-width: 768px) {
+    width: 12rem; /* Reduz o tamanho da imagem em telas pequenas */
+    height: 12rem;
+
+  }`;
 
 const Resumo = styled.div`
 position: absolute;
@@ -56,7 +86,16 @@ opacity: 0;
 padding-left:7px;
 padding-right:7px;
 
-
+@media (max-width: 768px) {
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+  width: 11.4rem; /* Reduz a largura do resumo em telas pequenas */
+  height: 12rem; /* Reduz a altura do resumo */ 
+  font-size: 0.73rem; /* Ajusta o tamanho da fonte em telas pequenas */
+  padding-left: 8px;
+padding-right: 8px;
+   }
 `;
 
 const PersonagemComHover = styled(Personagem)`
@@ -75,40 +114,77 @@ const Dados = styled.div`
 display:flex;
 flex-direction:column;
 align-items: center;
-`
+
+@media (max-width: 768px) {
+  display:flex;
+  font-size: 0.8rem; /* Ajusta o tamanho da fonte em telas pequenas */
+  width: 12rem;
+  text-align: center;
+  align-items: center;
+}`;
 
 const Nome = styled.a`
   font-size: 2rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    display:flex;
+    font-size: 1.5rem; /* Reduz o tamanho da fonte do nome */
+  }
 `;
 
 const Descricao = styled.p`
   font-size: 0.9rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem; /* Reduz o tamanho da fonte da descrição */
+  }
 `;
+
 const Quantidade = styled.p`
   font-size: 0.9rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem; /* Reduz o tamanho da fonte da quantidade */
+  }
 `;
 
 const CafeManha = styled.p`
   font-size: 0.9rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem; /* Reduz o tamanho da fonte do café da manhã */
+  }
 `;
 
 const Almoço = styled.p`
   font-size: 0.9rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem; /* Reduz o tamanho da fonte do almoço */
+  }
 `;
 
 const CafeTarde = styled.p`
   font-size: 0.9rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem; /* Reduz o tamanho da fonte do café da tarde */
+  }
 `;
 
 const Link = styled.p`
   font-size: 0.9rem;
   color: #333;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem; /* Reduz o tamanho da fonte do link */
+  }
 `;
 
 
